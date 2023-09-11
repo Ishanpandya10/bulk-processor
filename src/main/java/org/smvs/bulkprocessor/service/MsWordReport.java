0b556cb81cc5c43125240d0358ed8a3dba9e176e
Ishan Pandya <ishanpandya10@gmail.com>
2023-09-11 01:08:20 +0100

    Updated formatting and parsed input

--- src/main/java/org/smvs/bulkprocessor/service/MsWordReport.java
+++ src/main/java/org/smvs/bulkprocessor/service/MsWordReport.java
@@ -2,7 +2,10 @@ package org.smvs.bulkprocessor.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.poi.wp.usermodel.HeaderFooterType;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFFooter;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
@@ -15,9 +18,7 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTHMerge;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPageMar;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPageSz;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblLayoutType;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.STMerge;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.STTblLayoutType;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -34,6 +35,15 @@ import java.util.stream.IntStream;
 
 public class MsWordReport {
 
+    private List<String> headerKey;
+    private List<String> headerName;
+    private List<String> columnWidth;
+    private boolean landscape;
+    private String topHeader;
+    private String subHeaderKey;
+    private String methodName;
+    private boolean isNewPage;
+
     static List<String> columnNames = Arrays.asList("Member Id", "FID", "SG", "SC", "AG", "AP", "Full Name", "Mobile", "Whatsapp", "Gender", "House No");
     static List<String> columnKeys = Arrays.asList("ID", "FAMILY_ID", "SATSANG_GRADE", "SATSANG_CATEGORY", "AGE_GRADE", "AGE", "FULL_NAME", "MOBILE", "MOBILE", "GENDER", "FLAT_HOUSE_NO");
     //static List<String> rowHeaders = Arrays.asList("MemberID","FID","SG","SC","AG","FULL NAME", "FULL ADDRESS", "MOBILE", "CENTER");
@@ -46,12 +56,53 @@ public class MsWordReport {
     */
     private static final int numCols = columnNames.size();
 
+    public MsWordReport(JSONObject inputJsonData) {
+        extractInputJson(inputJsonData);
+    }
+
     public static void main(String[] args) throws IOException {
+        MsWordReport msWordReport = new MsWordReport(getInputJSON());
         String jsonString = String.join("", Files.readAllLines(Paths.get("E:\\Projects\\Smvs-projects\\bulk-processor\\src\\main\\java\\org\\smvs\\bulkprocessor\\service\\test.json")));
         JSONObject jsonObject = new JSONObject(jsonString);
         JSONArray data = jsonObject.getJSONArray("DATA");
-        //exportToWordReport(data);
-        //List<Object> objects = data.toList();
+        msWordReport.exportReportToWord(data);
+    }
+
+    private void extractInputJson(JSONObject jsonObject) {
+        String headerKey = jsonObject.getString("headerKey");
+        this.headerKey = getListFromCommaSeparatedString(headerKey);
+
+        String headerName = jsonObject.getString("headerName");
+        this.headerName = getListFromCommaSeparatedString(headerName);
+
+        String width = jsonObject.getString("columnWidth");
+        this.columnWidth = getListFromCommaSeparatedString(width);
+
+        this.landscape = jsonObject.getBoolean("landscape");
+
+        this.topHeader = jsonObject.getString("topHeader");
+
+        this.subHeaderKey = jsonObject.optString("subHeaderKey", "");
+        this.methodName = jsonObject.getString("methodName");
+
+        this.isNewPage = jsonObject.getBoolean("newPage");
+
+    }
+
+    private static List<String> getListFromCommaSeparatedString(String width) {
+        return Arrays.asList(width.split(","));
+    }
+
+    private static JSONObject getInputJSON() {
+        try {
+            String inputJson = String.join(" ", Files.readAllLines(Paths.get("src/main/java/org/smvs/bulkprocessor/service/input_config.json")));
+            return new JSONObject(inputJson);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void exportReportToWord(JSONArray data) throws IOException {
 
         ObjectMapper objectMapper = new ObjectMapper();
         List<HashMap<String, String>> allData = objectMapper.readValue(data.toString(), new TypeReference<>() {
@@ -70,35 +121,35 @@ public class MsWordReport {
         System.out.println("twoLevelGrouping: " + twoLevelGrouping);
 
         exportTwoLevelGroupingToWordReport(twoLevelGrouping);
-
-
     }
 
 
-    private static void exportOneLevelGroupingToWordReport(Map<String, List<HashMap<String, String>>> data) throws IOException {
+    private void exportOneLevelGroupingToWordReport(Map<String, List<HashMap<String, String>>> data) throws IOException {
         XWPFDocument document = new XWPFDocument();
         //document.enforceReadonlyProtection();
         setDocumentMargins(document);
+        setDocumentFooter(document, "lalitB");
         createTablesWithOneLevelGrouping(document, data);
 
-        FileOutputStream out = new FileOutputStream("report.docx");
+        FileOutputStream out = new FileOutputStream("report_one_level.docx");
         document.write(out);
         out.close();
 
     }
 
-    private static void exportTwoLevelGroupingToWordReport(Map<String, Map<String, List<HashMap<String, String>>>> twoLevelGrouping) throws IOException {
+    private void exportTwoLevelGroupingToWordReport(Map<String, Map<String, List<HashMap<String, String>>>> twoLevelGrouping) throws IOException {
         XWPFDocument document = new XWPFDocument();
         //document.enforceReadonlyProtection();
         setDocumentMargins(document);
+        setDocumentFooter(document, "lalitB");
         createTablesWithTwoLevelGrouping(document, twoLevelGrouping);
 
-        FileOutputStream out = new FileOutputStream("report2.docx");
+        FileOutputStream out = new FileOutputStream("report_two_level.docx");
         document.write(out);
         out.close();
     }
 
-    private static void setDocumentMargins(XWPFDocument document) {
+    private void setDocumentMargins(XWPFDocument document) {
         CTSectPr sectPr = document.getDocument().getBody().getSectPr();
         if (sectPr == null) sectPr = document.getDocument().getBody().addNewSectPr();
         CTPageMar pageMar = sectPr.getPgMar();
@@ -119,7 +170,37 @@ public class MsWordReport {
         pageSize.setH(BigInteger.valueOf(842 * 20));
     }
 
-    private static void createTablesWithOneLevelGrouping(XWPFDocument document, Map<String, List<HashMap<String, String>>> data) throws JSONException {
+    private void setDocumentFooter(XWPFDocument document, String currentUser) {
+        XWPFFooter footer = document.createFooter(HeaderFooterType.DEFAULT);
+
+        XWPFParagraph paragraph = footer.getParagraphArray(0);
+        if (paragraph == null) paragraph = footer.createParagraph();
+        paragraph.setAlignment(ParagraphAlignment.LEFT);
+        //addTabStop(paragraph, "CENTER", 3.25);
+
+        XWPFRun run = paragraph.createRun();
+        run.setText("Time: ");
+        paragraph.getCTP().addNewFldSimple().setInstr("TIME \\@ \"dd.MM.yyyy HH:mm:ss\" \\* MERGEFORMAT");
+
+        run = paragraph.createRun();
+        run.addTab();
+
+        run = paragraph.createRun();
+        run.setText("User: " + currentUser);
+
+        run = paragraph.createRun();
+        run.addTab();
+
+        run = paragraph.createRun();
+        run.setText("Page ");
+        paragraph.getCTP().addNewFldSimple().setInstr("PAGE \\* MERGEFORMAT");
+        run = paragraph.createRun();
+        run.setText(" of ");
+        paragraph.getCTP().addNewFldSimple().setInstr("NUMPAGES \\* MERGEFORMAT");
+
+    }
+
+    private void createTablesWithOneLevelGrouping(XWPFDocument document, Map<String, List<HashMap<String, String>>> data) throws JSONException {
 
         for (Map.Entry<String, List<HashMap<String, String>>> record : data.entrySet()) {
             List<HashMap<String, String>> firstGroup = record.getValue();
@@ -133,6 +214,7 @@ public class MsWordReport {
             XWPFTableRow groupRow = table.getRow(rowNum.getAndIncrement());
             XWPFTableCell groupCell = groupRow.getCell(0);
             groupCell.setText(record.getKey());
+            setTopHeaderTextFormat(groupCell);
             //spanCellsAcrossRow(table, rowNum.get(), 0, columnKeys.size());
 
             addTableHeaders(table, rowNum);
@@ -160,7 +242,7 @@ public class MsWordReport {
         //tableWidth.setW(BigInteger.valueOf(11400)); // Set table width to 5000 twips (1 inch = 1440 twips)
     }
 
-    private static void createTablesWithTwoLevelGrouping(XWPFDocument document, Map<String, Map<String, List<HashMap<String, String>>>> data) throws JSONException {
+    private void createTablesWithTwoLevelGrouping(XWPFDocument document, Map<String, Map<String, List<HashMap<String, String>>>> data) throws JSONException {
 
         for (Map.Entry<String, Map<String, List<HashMap<String, String>>>> groupRecord : data.entrySet()) {
             boolean flag = true;
@@ -174,12 +256,14 @@ public class MsWordReport {
                     XWPFTableRow firstGroupRow = table.getRow(rowNum.getAndIncrement());
                     XWPFTableCell firstGroupCell = firstGroupRow.getCell(0);
                     firstGroupCell.setText(groupRecord.getKey());
-
+                    setTopHeaderTextFormat(firstGroupCell);
                 }
 
                 XWPFTableRow secondGroupRow = table.getRow(rowNum.getAndIncrement());
                 XWPFTableCell secondGroupCell = secondGroupRow.getCell(0);
                 secondGroupCell.setText(record.getKey());
+                secondGroupCell.getParagraphs().get(0).getRuns().get(0).setBold(true);
+                secondGroupCell.getParagraphs().get(0).getRuns().get(0).setFontSize(13);
                 //spanCellsAcrossRow(table, rowNum.get(), 0, columnKeys.size());
 
                 addTableHeaders(table, rowNum);
@@ -210,15 +294,13 @@ public class MsWordReport {
 
     }
 
-    private static void spanCellsAcrossRow(XWPFTable table, int rowNum) {
+    private void setTopHeaderTextFormat(XWPFTableCell tableCell) {
+        tableCell.getParagraphs().get(0).setAlignment(ParagraphAlignment.CENTER);
+        tableCell.getParagraphs().get(0).getRuns().get(0).setBold(true);
+        tableCell.getParagraphs().get(0).getRuns().get(0).setFontSize(15);
+    }
 
-        /*IntStream.range(1, 3)
-                .forEach(value -> {
-                    XWPFTableRow row = table.getRow(rowNum);
-                    XWPFTableCell removed = row.getCell(value);
-                    removed.getCTTc().newCursor().removeXml();
-                    row.removeCell(value);
-                });*/
+    private void spanCellsAcrossRow(XWPFTable table, int rowNum) {
 
         CTHMerge hMerge = CTHMerge.Factory.newInstance();
         hMerge.setVal(STMerge.RESTART);
@@ -229,53 +311,13 @@ public class MsWordReport {
             hMerge.setVal(STMerge.CONTINUE);
         }
 
-
-        // First Row
-       /* CTHMerge hMerge = CTHMerge.Factory.newInstance();
-        hMerge.setVal(STMerge.RESTART);
-        XWPFTableCell cell = table.getRow(0).getCell(0);
-        addTcPr(cell);
-        cell.getCTTc().getTcPr().setHMerge(hMerge);*/
-
-       /* XWPFTableCell cell1 = table.getRow(1).getCell(0);
-        addTcPr(cell1);
-        cell1.getCTTc().getTcPr().setHMerge(hMerge);*/
-
-// Secound Row cell will be merged/"deleted"
-        //CTHMerge hMerge1 = CTHMerge.Factory.newInstance();
-      /*  hMerge.setVal(STMerge.CONTINUE);
-        XWPFTableCell cell2 = table.getRow(0).getCell(1);
-        addTcPr(cell2);
-        cell2.getCTTc().getTcPr().setHMerge(hMerge);*/
-
-        /*XWPFTableCell cell3 = table.getRow(1).getCell(1);
-        addTcPr(cell3);
-        cell3.getCTTc().getTcPr().setHMerge(hMerge1);*/
-
-       /* XWPFTableCell cell = table.getRow(rowNum).getCell(colNum);
-        if (cell.getCTTc().getTcPr() == null) cell.getCTTc().addNewTcPr();
-        if (cell.getCTTc().getTcPr().getGridSpan() == null) cell.getCTTc().getTcPr().addNewGridSpan();
-        cell.getCTTc().getTcPr().getGridSpan().setVal(BigInteger.valueOf(span));*/
-
-        /*IntStream.range(1, columnKeys.size())
-                .forEach(value -> {
-                    XWPFTableRow row = table.getRow(rowNum);
-                    //row.removeCell(value);
-                    XWPFTableCell removed = row.getCell(colNum);
-                    removed.getCTTc().newCursor().removeXml();
-                    row.removeCell(value);
-                });*/
-
-
-
-
     }
 
     private static void addTcPr(XWPFTableCell cell1) {
         if (cell1.getCTTc().getTcPr() == null) cell1.getCTTc().addNewTcPr();
     }
 
-    private static void addTableHeaders(XWPFTable table, AtomicInteger rowNum) {
+    private void addTableHeaders(XWPFTable table, AtomicInteger rowNum) {
         XWPFTableRow tableRow = table.getRow(rowNum.getAndIncrement());
         IntStream.range(0, columnNames.size())
                 .forEach(col -> {
@@ -297,41 +339,20 @@ public class MsWordReport {
                 });
     }
 
-    private static void addSpace(XWPFDocument document) {
+    private void addSpace(XWPFDocument document) {
         XWPFParagraph paragraph = document.createParagraph();
         XWPFRun xwpfRun = paragraph.createRun();
         xwpfRun.addBreak();
-
-        /*XWPFTable table123 = document.getTableArray(0);
-        org.apache.xmlbeans.XmlCursor cursor = table123.getCTTbl().newCursor();
-        cursor.toEndToken(); //now we are at end of the CTTbl
-        //there always must be a next start token. Either a p or at least sectPr.
-        while (cursor.toNextToken() != org.apache.xmlbeans.XmlCursor.TokenType.START) ;
-        XWPFParagraph newParagraph = document.insertNewParagraph(cursor);
-        newParagraph.createRun();*/
     }
 
-    private static void removeParagraphSpacing(XWPFParagraph paragraph) {
-        paragraph.setSpacingBefore(0);
-        paragraph.setSpacingBeforeLines(0);
-        paragraph.setSpacingAfter(0);
-        paragraph.setSpacingAfterLines(0);
-    }
-
-    private static XWPFTable createTable(XWPFDocument document, int numRows) {
+    private XWPFTable createTable(XWPFDocument document, int numRows) {
         XWPFTable table = document.createTable(numRows, numCols);
         table.setCellMargins(0, 0, 0, 0);
-        CTTblLayoutType type = table.getCTTbl().getTblPr().addNewTblLayout();
+        table.setWidth("100%");
+        //CTTblLayoutType type = table.getCTTbl().getTblPr().addNewTblLayout();
         //type.setType(STTblLayoutType.FIXED);
-        type.setType(STTblLayoutType.AUTOFIT);
-
-        //table.removeBorders();
+        //type.setType(STTblLayoutType.AUTOFIT);
         return table;
     }
 
-    private static String getFullName(JSONObject jsonObject) throws JSONException {
-        return String.join(" ", jsonObject.getString("FIRST_NAME_GUJ"),
-                jsonObject.getString("MIDDLE_NAME_GUJ"),
-                jsonObject.getString("LAST_NAME_GUJ"));
-    }
 }
